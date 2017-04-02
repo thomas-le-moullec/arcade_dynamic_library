d9ac8ae7d4b89dc32a457234c546763d234c8a32
Thomas Le-moullec <thomas.le-moullec@epitech.eu>
2017-04-02 17:45:10 +0200

    NEW RETRO WAAAAAAVE #Tsunami cyberpunk

--- lib/Includes/LSfml.hpp
+++ lib/Includes/LSfml.hpp
@@ -6,7 +6,7 @@
 #include <SFML/Graphics.hpp>
 
 #define WIDTH_WIN 1200
-#define HEIGHT_WIN 980
+#define HEIGHT_WIN 720
 #define PIXELS_WIN 24
 
 namespace arcade
@@ -32,6 +32,8 @@ namespace arcade
       void                                               initGameInputs();
       void                                               initCoreInputs();
       void                                               setColor(const unsigned int &, arcade::TileType, sf::RectangleShape);
+      void                                               print_commands() const;
+      std::string										                     cutName(std::string &, int) const;
       sf::RenderWindow                                   *_window;
       sf::Event                                           _event;
       bool                                                _fullScreen;
@@ -43,6 +45,8 @@ namespace arcade
       sf::Texture                                         _textureEmpty;
       sf::Texture                                         _textureBlock;
       sf::Texture                                         _texturePower;
+      sf::Texture                                         _textureBackgroundGame;
+      sf::Font                                            _fontMasque;
       sf::Font                                            _fontArial;
   };
 };
