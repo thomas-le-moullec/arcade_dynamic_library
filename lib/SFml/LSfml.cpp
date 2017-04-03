6a9941067eea8c9a9e148be86b0be6439c589b36
leo Le Diouron <leo.le-diouron@epitech.eu>
2017-04-03 09:39:49 +0200

    lsfml correct

--- lib/SFml/LSfml.cpp
+++ lib/SFml/LSfml.cpp
@@ -227,7 +227,7 @@ void										arcade::LSfml::ShowMenu(std::vector<std::string> gamesLibs, int id
   _window->display();
 }
 
-void									ShowScoreboard(std::string &nameGame, std::vector<arcade::Score> score)
+void										arcade::LSfml::ShowScoreboard(std::string &nameGame, std::vector<arcade::Score> score)
 {
   (void)nameGame;
   (void)score;
